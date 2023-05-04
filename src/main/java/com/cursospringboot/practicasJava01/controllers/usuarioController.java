6494999be4e29d38fafce81619157a50a73bf192
Moyrasc <noemisolac@gmail.com>
2023-05-04 11:07:50 +0200

    create method RequestMapping

--- src/main/java/com/cursospringboot/practicasJava01/controllers/usuarioController.java
+++ src/main/java/com/cursospringboot/practicasJava01/controllers/usuarioController.java
@@ -1,5 +1,7 @@
 package com.cursospringboot.practicasJava01.controllers;
 
+import com.cursospringboot.practicasJava01.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,41 @@ import java.util.List;
 public class usuarioController {
     //relaciona un método con una petición http
     @RequestMapping(value = "usuario")
-    public List<String> usuario(){
-        return List.of("user");
+    public Usuario getUsuario(@PathVariable Long id) {
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Noe");
+        usuario.setApellido("Sola");
+        usuario.setEmail("noemisolac@gmail.com");
+        usuario.setPhone("666666666");
+        return usuario;
     }
+    @RequestMapping(value = "usuario2")
+    public Usuario editarUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Noe");
+        usuario.setApellido("Sola");
+        usuario.setEmail("noemisolac@gmail.com");
+        usuario.setPhone("666666666");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario3")
+    public Usuario borrarUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Noe");
+        usuario.setApellido("Sola");
+        usuario.setEmail("noemisolac@gmail.com");
+        usuario.setPhone("666666666");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario4")
+    public Usuario encontarUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Noe");
+        usuario.setApellido("Sola");
+        usuario.setEmail("noemisolac@gmail.com");
+        usuario.setPhone("666666666");
+        return usuario;
+    }
+
 }
