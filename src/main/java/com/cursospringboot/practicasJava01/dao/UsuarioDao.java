dbec750903f7a90a5e70b21ebe48bea790deef67
Moyrasc <noemisolac@gmail.com>
2023-05-04 11:59:53 +0200

    create dao interface

---
+++ src/main/java/com/cursospringboot/practicasJava01/dao/UsuarioDao.java
@@ -0,0 +1,9 @@
+package com.cursospringboot.practicasJava01.dao;
+
+import com.cursospringboot.practicasJava01.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+    List<Usuario>getUsuarios();
+}
